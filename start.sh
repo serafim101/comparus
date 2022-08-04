35ecb56fcf807f0c5c4c379fbe694c4b1e0092ff
Константин Мальованый <konstantin.malovanyj@privatbank.ua>
2022-08-04 10:44:08 +0300

    Add manifests

--- start.sh
+++ start.sh
@@ -59,37 +59,37 @@ fi
 
 
 echo "Start minikube cluster"
-minikube start --vm-driver='docker' --cpus='4' --memory='8g'
-minikube addons enable ingress
+sudo minikube start --vm-driver='docker' --cpus='4' --memory='8g'
+sudo minikube addons enable ingress
 
 echo "Preparing minikube cluster for jenkins server"
-minikube ssh -- "sudo mkdir /data/jenkins-volume"
-minikube ssh -- "sudo chown -R 1000:1000 /data/jenkins-volume"
-minikube ssh -- "sudo chmod 666 /var/run/docker.sock"
+sudo minikube ssh -- "sudo mkdir /data/jenkins-volume"
+sudo minikube ssh -- "sudo chown -R 1000:1000 /data/jenkins-volume"
+sudo minikube ssh -- "sudo chmod 666 /var/run/docker.sock"
 
 echo "Install Jenkins to kubernetes"
-kubectl create namespace jenkins
-kubectl apply -f $PWD/jenkins-volume.yaml
-kubectl apply -f $PWD/jenkins-sa.yaml
+sudo kubectl create namespace jenkins
+sudo kubectl apply -f $PWD/jenkins-volume.yaml
+sudo kubectl apply -f $PWD/jenkins-sa.yaml
 
 helm repo add jenkinsci https://charts.jenkins.io
 helm repo update
-helm install jenkins -n jenkins -f $PWD/values.yaml jenkinsci/jenkins --wait
+sudo helm install jenkins -n jenkins -f $PWD/values.yaml jenkinsci/jenkins --wait
 
-kubectl apply -f $PWD/jenkins-ingress.yaml --wait
+sudo kubectl apply -f $PWD/jenkins-ingress.yaml --wait
 
-JEN_PASS=$(kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/additional/chart-admin-password && echo)
-JEN_USER=$(kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/additional/chart-admin-username && echo)
-IP=$(minikube ip)
+JEN_PASS=$(sudo kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/additional/chart-admin-password && echo)
+JEN_USER=$(sudo kubectl exec --namespace jenkins -it svc/jenkins -c jenkins -- /bin/cat /run/secrets/additional/chart-admin-username && echo)
+IP=$(sudo minikube ip)
 
-cat <<EOF | tee -a /etc/hosts
+cat <<EOF | sudo tee -a /etc/hosts
 ${IP} ${JENKINS_DOMAIN}
 EOF
 
 sleep 5
 
 echo "Deploy jenkins job to server with terraform"
-terraform -chdir=$PWD/terraform/jenkins apply -var "jenkins_url=http://$JENKINS_DOMAIN" -var "jen_user=$JEN_USER" -var "jen_pass=$JEN_PASS" -auto-approve
+sudo terraform -chdir=$PWD/terraform/jenkins apply -var "jenkins_url=http://$JENKINS_DOMAIN" -var "jen_user=$JEN_USER" -var "jen_pass=$JEN_PASS" -auto-approve
 
 echo ""
 echo "Ingress URL: http://$JENKINS_DOMAIN"
