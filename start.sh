6c2b775ef5ddfb17e81e1e281129c4afaa0d13a7
Константин Мальованый <konstantin.malovanyj@privatbank.ua>
2022-08-06 10:21:56 +0300

    Bugfix in start.sh

--- start.sh
+++ start.sh
@@ -5,14 +5,66 @@ JENKINS_DOMAIN="jenkins.lc"
 PWD=$(pwd)
 
 # Script functions
-function usage() {
+usage() {
 	echo "Use the following options to start deployment:"
 	echo " For Helm deploying: sh $0 helm"
 	echo " For Terraform deploing: sh $0 terraform"
 }
 
-function deploy_helm() {
+k8s_prepair() {
+	if ![[ -d "/usr/local/bin/"]]; then
+		sudo mkdir -p /usr/local/bin/
+	fi
+
+	if [[ $(minikube version) ]]; then
+		echo "Minikube is installed in system"
+	else
+		echo "Minikube is not present in system. Installing"
+		curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+		chmod +x minikube
+		sudo mv minikube /usr/local/bin/
+	fi
+
+	if [[ $(kubectl version --short) ]]; then
+		echo "Kubectl is installed in system"
+	else
+		echo "Kubectl is not installed in system. Installing"
+		curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
+		chmod +x ./kubectl
+		sudo mv ./kubectl /usr/local/bin/kubectl
+	fi
+
+	if [[ $(terraform version) ]]; then
+		echo "Terraform is installed in system"
+	else
+		echo "Terraform is not installed in system. Installing"
+		wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
+		echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+		sudo apt update && sudo apt install terraform
+	fi
+
+
+	echo "Start minikube cluster"
+	minikube start --vm-driver='docker' --cpus='4' --memory='8g'
+	minikube addons enable ingress
+
+	echo "Preparing minikube cluster for jenkins server"
+	minikube ssh -- "sudo mkdir /data/jenkins-volume"
+	minikube ssh -- "sudo chown -R 1000:1000 /data/jenkins-volume"
+	minikube ssh -- "sudo chmod 666 /var/run/docker.sock"
+
+	if [[ $(cat /etc/hosts | grep "$JENKINS_DOMAIN" | awk '{print $2}') == "$JENKINS_DOMAIN" ]]; then
+		IP=$(minikube ip)
+		echo "Add minikube ip-address: $IP to /etc/hosts, for associate IP with INGRESS DOMAIN"
+		cat <<EOF | sudo tee -a /etc/hosts
+${IP} ${JENKINS_DOMAIN}
+EOF
+	fi
+}
+
+deploy_helm() {
 	echo "Install Jenkins to kubernetes with helm chart"
+	k8s_prepair
 	kubectl create namespace jenkins
 	kubectl apply -f $PWD/k8s/jenkins-volume.yaml
 	kubectl apply -f $PWD/k8s/jenkins-sa.yaml
@@ -49,8 +101,9 @@ function deploy_helm() {
 	echo "Password: $JEN_PASS"
 }
 
-function deploy_terraform() {
+deploy_terraform() {
 	echo "Install Jenkins to kubernetes with terraform"
+	k8s_prepair
 	terraform -chdir=$PWD/terraform/k8s init
 	terraform -chdir=$PWD/terraform/k8s apply -var "ingress_domain=$JENKINS_DOMAIN" -auto-approve
 
@@ -70,56 +123,6 @@ function deploy_terraform() {
 	echo "Uername: $JEN_USER"
 	echo "Password: $JEN_PASS"
 }
-
-
-if ![[ -d "/usr/local/bin/"]]; then
-	sudo mkdir -p /usr/local/bin/
-fi
-
-if [[ $(minikube version) ]]; then
-	echo "Minikube is installed in system"
-else
-	echo "Minikube is not present in system. Installing"
-	curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-	chmod +x minikube
-	sudo mv minikube /usr/local/bin/
-fi
-
-if [[ $(kubectl version --short) ]]; then
-	echo "Kubectl is installed in system"
-else
-	echo "Kubectl is not installed in system. Installing"
-	curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl
-	chmod +x ./kubectl
-	sudo mv ./kubectl /usr/local/bin/kubectl
-fi
-
-if [[ $(terraform version) ]]; then
-	echo "Terraform is installed in system"
-else
-	echo "Terraform is not installed in system. Installing"
-	wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
-	echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-	sudo apt update && sudo apt install terraform
-fi
-
-
-echo "Start minikube cluster"
-minikube start --vm-driver='docker' --cpus='4' --memory='8g'
-minikube addons enable ingress
-
-echo "Preparing minikube cluster for jenkins server"
-minikube ssh -- "sudo mkdir /data/jenkins-volume"
-minikube ssh -- "sudo chown -R 1000:1000 /data/jenkins-volume"
-minikube ssh -- "sudo chmod 666 /var/run/docker.sock"
-
-if [[ $(cat /etc/hosts | grep "$JENKINS_DOMAIN" | awk '{print $2}') == "$JENKINS_DOMAIN" ]]; then
-	IP=$(minikube ip)
-	echo "Add minikube ip-address: $IP to /etc/hosts, for associate IP with INGRESS DOMAIN"
-	cat <<EOF | sudo tee -a /etc/hosts
-${IP} ${JENKINS_DOMAIN}
-EOF
-fi
 
 
 case $1 in
